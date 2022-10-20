96097e7462e61df92836955a1b5dbaab0fb9df37
Guillaume Ducuing <ducuingg@yahoo.fr>
2022-10-20 17:28:47 +0200

    update

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,12 +1,12 @@
-export const Footer = () => import('../../components/Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
-export const Form = () => import('../../components/Form.vue' /* webpackChunkName: "components/form" */).then(c => wrapFunctional(c.default || c))
-export const Header = () => import('../../components/Header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
-export const Hero = () => import('../../components/Hero.vue' /* webpackChunkName: "components/hero" */).then(c => wrapFunctional(c.default || c))
-export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
-export const Projects = () => import('../../components/Projects.vue' /* webpackChunkName: "components/projects" */).then(c => wrapFunctional(c.default || c))
-export const Services = () => import('../../components/Services.vue' /* webpackChunkName: "components/services" */).then(c => wrapFunctional(c.default || c))
-export const Skills = () => import('../../components/Skills.vue' /* webpackChunkName: "components/skills" */).then(c => wrapFunctional(c.default || c))
-export const Tutorial = () => import('../../components/Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
+export { default as Footer } from '../../components/Footer.vue'
+export { default as Form } from '../../components/Form.vue'
+export { default as Header } from '../../components/Header.vue'
+export { default as Hero } from '../../components/Hero.vue'
+export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
+export { default as Projects } from '../../components/Projects.vue'
+export { default as Services } from '../../components/Services.vue'
+export { default as Skills } from '../../components/Skills.vue'
+export { default as Tutorial } from '../../components/Tutorial.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
